a65235e13f6bfe72dcb5bb6bb83b25239d5166f9
lucaxartx <ujude1000@gmail>
2023-11-28 19:09:43 +0100

    fix: fixed logout route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,6 @@ const { register, login, logout } = require("../controllers/authController");
 
 router.route("/register").post(register);
 router.route("/register").post(login);
-router.route("/register").post(logout);
+router.route("/register").get(logout);
 
 module.exports = router;
