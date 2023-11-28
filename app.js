906e905bb0d8649dee5ea174633c1360677577ac
lucaxartx <ujude1000@gmail>
2023-11-28 08:42:21 +0100

    feat:added db folder

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
-
 const app = express();
+
+const connectDb = require("./src/db/connect");
 
 app.get("/", (req, res) => {
   res.send("my homepage");
@@ -13,6 +14,7 @@ const port = process.env.PORT || 3000;
 
 const start = async function () {
   try {
+    await connectDb(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`server is live on port:${port}`);
     });
