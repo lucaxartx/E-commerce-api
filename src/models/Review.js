83d2cf8c390dbf547a55b04a3714a1c9b52599fd
lucaxartx <ujude1000@gmail>
2023-12-17 16:56:36 +0100

    feat:added auto update for numOfReviews and avgRating

--- src/models/Review.js
+++ src/models/Review.js
@@ -28,4 +28,43 @@ const reviewSchema = mongoose.Schema(
 );
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+// auto update average rating and numOfReviews
+reviewSchema.statics.calculateAverageRating = async function (productId) {
+  console.log(productId);
+  const result = this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  console.log(result);
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// when we use the update controller
+ReviewSchema.post("save", async function () {
+  console.log("post save hook called ");
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+//when we use the delete controller
+ReviewSchema.post("remove", async function () {
+  console.log("post remove hook called ");
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 module.exports = mongoose.model("Review", reviewSchema);
